17b5196e2429ef71a4dd4eefe1076f9b82f248de
Justin Kamerman <34698728+JustinFromMCPE@users.noreply.github.com>
2018-01-16 17:29:53 -0600

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ client.on('message', msg => {
     if (!msg.content.startsWith(process.env.PREFIX) || !msg.guild) return;
     const command = msg.content.split(' ')[0].substr(process.env.PREFIX.length);
     const args = msg.content.split(' ').slice(1).join(' ');
-    if (command === 'guide') return msg.channel.send('https://git.io/d.js-heroku');
+    if (command === 'help') return msg.channel.send('This bot is currently undergoing maintenance');
     else if (command === 'invite') return msg.channel.send(process.env.INVITE);
 });
 
